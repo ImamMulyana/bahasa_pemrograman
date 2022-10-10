ffdda940965ca129439e7d54294505359ce30680
root <root@LAPTOP-9EFHH6T4.localdomain>
2022-10-10 11:51:14 +0700

    bahasa_pemrograman

---
+++ type_data/variabel/src.py
@@ -0,0 +1,25 @@
+#variabel
+a = "Imam Mulyana"
+def func():
+    a = "any"
+    print ("selamat " + a)
+func()
+print(a)
+
+#definisi
+def tambah ():
+    a = 5
+    b = 3
+    c = a+b
+    print (c)
+
+tambah()
+
+#definisi parameter
+def data (nama, nim):
+    print ("nama saya {nama} dan {nim}")
+data ("dian", "123")
+
+#contoh
+def total (sisi):
+    return sisi*sisi
